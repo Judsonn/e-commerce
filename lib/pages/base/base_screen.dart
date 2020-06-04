0cd0ac6495cd3e1d15f735ef9779fdba04a30fec
Judson <judson.henrique01@gmail.com>
2020-06-04 16:19:35 -0300

    DrawerBloc more plugins
    global bloc for managing the drawer screens.
    Necessary plugins in the project

--- lib/pages/base/base_screen.dart
+++ lib/pages/base/base_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+import 'package:e_comerce/bloc/drawer_bloc.dart';
 import 'package:e_comerce/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class BaseScreen extends StatefulWidget {
   @override
@@ -12,9 +15,35 @@ class _BaseScreenState extends State<BaseScreen> {
 
 final PageController  _pageController =  PageController(); 
 
+  DrawerBloc _drawerBloc;
+  StreamSubscription _drawerSubscription;
+
+
+  @override
+//função para trocar as telas de acordo com o escolhido no drawer, o did foi usado pelo fato do bloc
+//pedir um context.,
+  void didChangeDependencies() {
+    
+    super.didChangeDependencies();
+
+    final DrawerBloc drawerBloc = Provider.of<DrawerBloc>(context);
+    if(drawerBloc != _drawerBloc){
+      _drawerBloc = drawerBloc;
+
+      _drawerSubscription?.cancel();
+      _drawerSubscription =  _drawerBloc.outPage.listen((page) {
+        _pageController.jumpToPage(page);
+       });
+    }
+   
+  }
+  void dispose(){
+    _drawerSubscription.cancel();
+    super.dispose();
+  }
+
 
   Widget build(BuildContext context) {
-    
   
     return Scaffold(
       body: PageView(
--- lib/root/root_app.dart
+++ lib/root/root_app.dart
@@ -1,29 +1,38 @@
+import 'package:e_comerce/bloc/drawer_bloc.dart';
 import 'package:e_comerce/pages/base/base_screen.dart';
-import 'package:e_comerce/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class RootApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.green[200],
-        inputDecorationTheme: InputDecorationTheme(
-          fillColor: Colors.white,
-          focusColor: Colors.white, 
-          contentPadding: EdgeInsets.all(8),
-          filled: true,
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide(style: BorderStyle.none),
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.0),
+    return MultiProvider(
+      providers: [
+        Provider<DrawerBloc>(
+          create: (_) => DrawerBloc(),
+          dispose: (context, value) => value.dispose(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.green[200],
+          inputDecorationTheme: InputDecorationTheme(
+            fillColor: Colors.white,
+            focusColor: Colors.white, 
+            contentPadding: EdgeInsets.all(8),
+            filled: true,
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              borderSide: BorderSide(style: BorderStyle.none),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
           ),
         ),
+        home: BaseScreen(),
       ),
-      home: BaseScreen(),
     );
   }
 }
