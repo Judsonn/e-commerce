e0432d0a8955c8b970be5caccccca0528938878e
Judson <judson.henrique01@gmail.com>
2020-06-05 13:29:37 -0300

    integrating the research field with the bloc

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:e_comerce/bloc/home_page_bloc.dart';
 import 'package:e_comerce/common/custom_drawer/custom_drawer.dart';
 import 'package:e_comerce/pages/home/widgets/search_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,30 +12,80 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
 
+HomeBloc _homeBloc ;
+
+@override
+void didChangeDependencies() {
+  super.didChangeDependencies();
+  
+  final HomeBloc homeBloc = Provider.of<HomeBloc>(context);
+  if(homeBloc != _homeBloc)
+    _homeBloc = homeBloc;
+}
+
+
+
   Widget build(BuildContext context) {
 
     _operSearch(String currentSearch) async {
      final String search = await showDialog(context: context,
       builder: (context) =>  SearchDialog(currentSearch: currentSearch), 
       );
+      if(search != null)
+        _homeBloc.setSearch(search);
     }
     
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
-       title: Text("E-commerce", style: TextStyle(
-         fontSize: 20,
-         color: Colors.white,
-         fontWeight: FontWeight.bold
-
-       ),),
+       title: StreamBuilder<String>(
+        stream: _homeBloc.outSearch,
+        //inicar o search com um texto vazio, pra não da problema de null
+        initialData: "",
+        builder: (context, snapshot){
+          //reconhece o que foi pesquisado e add no appBar
+          if(snapshot.data.isEmpty)
+          return Container();
+          else
+          //gesture para conseguir abrir a pesquisa clicando no texto 
+          return GestureDetector(
+            onTap: () => _operSearch(snapshot.data),
+            child: LayoutBuilder(
+              builder: (context, constraints){
+                //containe é pra definir o damanho do local clicavel, ele usa o constrains para 
+                //aproveitar o tamanho máximo clicavel
+                  return Container(
+                    child: Text(snapshot.data),
+                    width: constraints.biggest.width,
+                  );
+              } 
+              ),
+          );
+        },
+       ), 
          actions: <Widget>[
-           IconButton(
-             icon: Icon(Icons.search),
-             onPressed: (){
-               _operSearch("");
+           //trocando os icon de lupa para close depois de ter pesquisado alguma coisa 
+          StreamBuilder<String>(
+            stream: _homeBloc.outSearch,
+            initialData: "",
+            builder: (context, snapshot) {
+              if(snapshot.data.isEmpty)
+              return  IconButton(
+                icon: Icon(Icons.search),
+                onPressed: (){
+                _operSearch("");
+                },
+             );
+
+             else
+             return  IconButton(
+              icon: Icon(Icons.close),
+              onPressed: (){
+                _homeBloc.setSearch(''); 
              },
-             )
+             );
+            },
+          )
          ]
       ),
       drawer: CustomDrawer(),
--- lib/root/root_app.dart
+++ lib/root/root_app.dart
@@ -1,4 +1,5 @@
 import 'package:e_comerce/bloc/drawer_bloc.dart';
+import 'package:e_comerce/bloc/home_page_bloc.dart';
 import 'package:e_comerce/pages/base/base_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -6,13 +7,20 @@ import 'package:provider/provider.dart';
 class RootApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //blocs globais que podem ser acessados de todas as pages
     return MultiProvider(
       providers: [
         Provider<DrawerBloc>(
           create: (_) => DrawerBloc(),
           dispose: (context, value) => value.dispose(),
-        )
+        ),
+        Provider<HomeBloc>(
+          create: (_) => HomeBloc(),
+          dispose: (context, value) => value.dispose(),
+        ),
       ],
+
+      
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
